e9789721d88cd382f8880dff18d807544351fd93
Miloslav Jezek <miloslav.jezek@fpt.sk>
2023-09-16 11:25:47 +0200

    Set interface for access to a databse and create MongoDB implementation

---
+++ src/main/kotlin/eu/milek549/data/MongoDbDataSource.kt
@@ -0,0 +1,24 @@
+package eu.milek549.data
+
+import com.mongodb.kotlin.client.coroutine.MongoDatabase
+import eu.milek549.data.model.Message
+import kotlinx.coroutines.flow.toList
+
+/**
+ * Implementation for MongoDB.
+ * MongoDB will automatically create a collection of documents.
+ */
+class MessageDataSourceImpl(
+    private val db: MongoDatabase
+): MessageDataSource {
+
+    private val messages = db.getCollection<Message>("messages")
+
+    override suspend fun getAllMessages(): List<Message> {
+        return messages.find().toList().sortedByDescending(Message::timestamp)
+    }
+
+    override suspend fun insertMessage(message: Message) {
+        messages.insertOne(message)
+    }
+}
