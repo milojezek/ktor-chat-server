c323e1b71b15d35b170eeb7100819f657e6f7b21
Miloslav Jezek <miloslav.jezek@fpt.sk>
2023-09-16 12:42:48 +0200

    Set routing

--- src/main/kotlin/eu/milek549/plugins/Routing.kt
+++ src/main/kotlin/eu/milek549/plugins/Routing.kt
@@ -1,9 +1,17 @@
 package eu.milek549.plugins
 
+import eu.milek549.room.RoomController
+import eu.milek549.routes.chatSocket
+import eu.milek549.routes.getAllMessages
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
 
 fun Application.configureRouting() {
-
+    val roomController by inject<RoomController>()
+    install(Routing) {
+        chatSocket(roomController)
+        getAllMessages(roomController)
+    }
 }
